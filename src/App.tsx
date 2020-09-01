5f99f0210fb254749902229992d2fd514fd4dccc
Rayo Verweij <rayo.verweij@live.nl>
2020-09-01 14:32:19 +0100

    Change weeks

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,12 @@ import WeekDay from './calendar/WeekDay';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { getWeek, startOfWeek, addDays } from 'date-fns';
+import { ArrowLeftCircle, ArrowRightCircle, ArrowDownCircle } from 'react-bootstrap-icons';
+import { format, getWeek, startOfWeek, addDays, addWeeks, subWeeks } from 'date-fns';
 
 
 const App = () => {
-    const [currentDate, _] = useState(new Date());
+    const [currentDate, setCurrentDate] = useState(new Date());
 
     const weekStart = startOfWeek(currentDate, { weekStartsOn: 1 });
 
@@ -17,24 +18,40 @@ const App = () => {
         weekDays.push(addDays(weekStart, i));
     }
 
+    const prevWeek = () => {
+        setCurrentDate(subWeeks(currentDate, 1));
+    }
+
+    const nextWeek = () => {
+        setCurrentDate(addWeeks(currentDate, 1));
+    }
+
+    const today = () => {
+        setCurrentDate(new Date());
+    }
+
+
     return (
         <Container fluid>
             <header>
                 <Row>
                     <Col>
                         <h1>Agenda</h1>
-                        <span className="weekNumber">Week {getWeek(currentDate)}</span>
+                        <span className="weekNumber">Week {getWeek(currentDate)}, {format(currentDate, 'MMMM Y')}</span>
                     </Col>
                 </Row>
             </header>
             <main>
                 <Row className="daysOfWeek">
-                    <Col>
+                    <Col id="toolCol">
+                        <ArrowLeftCircle className="weekChevron" onClick={prevWeek} />
+                        <ArrowDownCircle className="weekChevron" onClick={today} />
+                        <ArrowRightCircle className="weekChevron" onClick={nextWeek} />
                     </Col>
                     {
-                        weekDays.map(weekDay => {
+                        weekDays.map((weekDay, index) => {
                             return (
-                                <WeekDay date={weekDay} />
+                                <WeekDay date={weekDay} key={`weekDay-${index}`} />
                             )
                         })
                     }
